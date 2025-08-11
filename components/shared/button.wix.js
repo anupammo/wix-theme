3980ac6a4507ebcf3c7023e421c3d6a9aa8f4f44
Anupam Mondal <a09051985@gmail.com>
2025-08-12 02:04:43 +0530

    Update button.wix.js

--- components/shared/button.wix.js
+++ components/shared/button.wix.js
@@ -1,1 +1,11 @@
 // Reusable button component
+// Reusable button hover effect
+$w.onReady(() => {
+  $w("#primaryButton").onMouseIn(() => {
+    $w("#primaryButton").style.backgroundColor = "#FF6F00";
+  });
+
+  $w("#primaryButton").onMouseOut(() => {
+    $w("#primaryButton").style.backgroundColor = "#1A73E8";
+  });
+});
