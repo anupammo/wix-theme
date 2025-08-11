b270fd18746d4b28e4becf28cc78d5dc132edd29
Anupam Mondal <a09051985@gmail.com>
2025-08-12 02:02:45 +0530

    Update navbar.wix.js

--- components/header/navbar.wix.js
+++ components/header/navbar.wix.js
@@ -1,6 +1,18 @@
-// Sticky nav, active link highlights component for Wix header
+// Sticky navbar and active link highlight
 $w.onReady(() => {
+  const currentPage = wixLocation.path[0];
+  $w("#navLinks").forEach(link => {
+    if (link.link.includes(currentPage)) {
+      link.style.color = "#1A73E8"; // Active color
+    }
+  });
+
+  // Mobile menu toggle
   $w("#menuToggle").onClick(() => {
-    $w("#mobileMenu").toggle();
+    $w("#mobileMenu").expand();
+  });
+
+  $w("#closeMenu").onClick(() => {
+    $w("#mobileMenu").collapse();
   });
 });
