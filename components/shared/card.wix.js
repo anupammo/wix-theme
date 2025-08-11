725d89223476a543043d37c12a7645bc527be4af
Anupam Mondal <a09051985@gmail.com>
2025-08-12 02:05:08 +0530

    Update card.wix.js

--- components/shared/card.wix.js
+++ components/shared/card.wix.js
@@ -1,1 +1,6 @@
-// Info or feature card component
+// Feature card reveal animation
+$w.onReady(() => {
+  $w("#featureCard").onViewportEnter(() => {
+    $w("#featureCard").show("fade", { duration: 600 });
+  });
+});
